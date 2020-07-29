831bd489e81c8d6d228f2ae7b5a3dcd65738a6e9
aron <pachl.aron@gmail.com>
2020-07-29 12:18:00 +0200

    worldmap

--- citystatistic/src/App.js
+++ citystatistic/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DefaultCities from "./components/DefaultCities";
+import WorldMap from "./components/WorldMap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Route exact path="/" component={DefaultCities} />
+        <Route exact path="/worldmap" component={WorldMap} />
       </Router>
     </div>
   );
