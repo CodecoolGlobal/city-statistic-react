76e173c6d192c467f38e4f0214716e3c4a2eae63
Jorakasi <gabor.olah0323@gmail.com>
2020-08-11 11:19:18 +0200

    changed the way continents endpoints works

--- citystatistic/src/components/CitiesByContinent.js
+++ citystatistic/src/components/CitiesByContinent.js
@@ -1,59 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import DisplayCity from "./DisplayCity";
-import { v4 as uuidv4 } from "uuid";
-import { Link } from "react-router-dom";
+import {v4 as uuidv4} from "uuid";
+import {Link} from "react-router-dom";
 
 export default function CitiesByContinent(props) {
-  let [cityData, setCityData] = useState([]);
-
-  useEffect(() => {
-    const apiPath = `http://localhost:8080/continent/${props.match.params.id}`;
-    axios.get(apiPath).then((res) => {
-      setCityData((oldCityData) => [...oldCityData, res.data]);
-    });
-  }, []);
-
-  if (cityData.length < 1) {
-    return (
-      <table id="wrapper">
-        <tr>
-          <td>
-            <img
-              id="loading"
-              src="/globe.gif"
-              alt="loading globe"
-              width="10%"
-              height="auto"
-            />
-          </td>
-        </tr>
-      </table>
-    );
-  } else {
-    return (
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(500px, max-content))",
-          justifyContent: "center",
-          gridGap: "3rem",
-          overFlow: "auto",
-          marginTop: "5%",
-          marginBottom: "2%",
+    let [cityData, setCityData] = useState([]);
+    const continents = "AF EU NA SA OC AS";
+    useEffect(() => {
+        if (!continents.includes(props.match.params.id)) {
+            window.location.href = "/";
+        } else {
+            const apiPath = `http://localhost:8080/continent/${props.match.params.id}`;
+            axios.get(apiPath).then((res) => {
+                setCityData((oldCityData) => [...oldCityData, res.data]);
+            });
         }}
-      >
-        {cityData[0].map((city) => (
-          <Link to={`/city/${city.citySlug}`}>
-            <DisplayCity
-              key={uuidv4()}
-              name={city.cityName}
-              image={city.cityImage}
-              slug={city.citySlug}
-            />
-          </Link>
-        ))}
-      </div>
+    ,
+        []
     );
-  }
+
+
+    if (cityData.length < 1) {
+        return (
+            <table id="wrapper">
+                <tr>
+                    <td>
+                        <img
+                            id="loading"
+                            src="/globe.gif"
+                            alt="loading globe"
+                            width="10%"
+                            height="auto"
+                        />
+                    </td>
+                </tr>
+            </table>
+        );
+
+    } else {
+        return (
+            <div
+                style={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(auto-fit, minmax(500px, max-content))",
+                    justifyContent: "center",
+                    gridGap: "3rem",
+                    overFlow: "auto",
+                    marginTop: "5%",
+                    marginBottom: "2%",
+                }}
+            >
+                {cityData[0].map((city) => (
+                    <Link to={`/city/${city.citySlug}`}>
+                        <DisplayCity
+                            key={uuidv4()}
+                            name={city.cityName}
+                            image={city.cityImage}
+                            slug={city.citySlug}
+                        />
+                    </Link>
+                ))}
+            </div>
+        );
+
+    }
+
 }
