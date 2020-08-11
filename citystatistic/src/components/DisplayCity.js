5a78d0dbf584379591ef6da714a94498a54a10fb
aron <pachl.aron@gmail.com>
2020-08-11 17:37:52 +0200

    show favourite star new title

--- citystatistic/src/components/DisplayCity.js
+++ citystatistic/src/components/DisplayCity.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Icon, InlineIcon } from "@iconify/react";
+import starFilled from "@iconify/icons-ant-design/star-filled";
+import starOutlined from "@iconify/icons-ant-design/star-outlined";
 
 export default function DisplayCity(props) {
+  let list = ["auckland"];
   return (
     <div className="card card-hover">
       <img className="card-img layer" src={props.image} alt={props.name} />
       <div className="card-img-overlay text-white d-flex flex-column justify-content-center">
         <h4 className="card-title">{props.name}</h4>
+      </div>
+      <div className="star">
+        {list.includes(props.name.toLowerCase()) ? (
+          <Icon icon={starFilled} color="#FFFFFF" width="80" height="80" />
+        ) : (
+          <Icon icon={starOutlined} color="#FFFFFF" width="80" height="80" />
+        )}
       </div>
     </div>
   );
