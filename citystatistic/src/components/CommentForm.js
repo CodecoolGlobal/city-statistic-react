a371d4d3f40640a0ff6f709dbf9889d958e80e90
Jorakasi <gabor.olah0323@gmail.com>
2020-08-12 16:21:21 +0200

    comment form post request created to add comments

--- citystatistic/src/components/CommentForm.js
+++ citystatistic/src/components/CommentForm.js
@@ -1,28 +1,22 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
+import axios from 'axios';
 
 export default function CommentForm(props) {
     let citySlug = props.citySlug;
 
     console.log(citySlug)
+
+    function handleSubmit() {
+        axios.post('http://localhost:8080/add-comment/budapest', {comment: 'budapest321'}).then(res => console.log(res))
+    }
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <textarea placeholder="Add your comment here" required/>
                 <button id="submit" type="submit">Submit
                 </button>
             </form>
         </div>
     );
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        const data = event.target;
-        fetch(`localhost:8080/add-comment/budapest`, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({comment: "event.target"}),
-        }).then(response => response.json());
-        console.log(data);
-    }
 
 }
