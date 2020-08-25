dde6433c5f5a33a1d462dfd5c743d1f622ab80b5
aron <pachl.aron@gmail.com>
2020-08-25 11:17:15 +0200

    remove login button for test

--- citystatistic/src/layout/Header.js
+++ citystatistic/src/layout/Header.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import SearchCity from "../components/SearchCity";
 import ActualTime from "../components/ActualTime";
-import axios from "axios";
 
 export default function Header() {
   const TopNav = styled.div`
@@ -27,27 +26,6 @@ export default function Header() {
     marginTop: "0.55%",
   };
 
-  function sendPost() {
-    axios
-      .post("http://localhost:8080/auth/signin", {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": "true",
-          "Access-Control-Request-Methods": "PUT,POST,GET,DELETE,OPTIONS",
-          "Access-Control-Allow-Headers":
-            "content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, content-type",
-          "Content-Type": "application/json",
-        },
-        data: {
-          username: "admin",
-          password: "password",
-        },
-      })
-      .then(function (response) {
-        console.log(response);
-      });
-  }
-
   return (
     <TopNav>
       <Link className="header-link" style={Links} to="/">
@@ -61,7 +39,6 @@ export default function Header() {
       </a>
       <ActualTime />
       <SearchCity />
-      <button onClick={sendPost}>log in</button>
     </TopNav>
   );
 }
