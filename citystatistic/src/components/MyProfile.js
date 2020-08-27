50df5f8a784200f93e845992a34a837065649364
aron <pachl.aron@gmail.com>
2020-08-27 19:21:36 +0200

    user profile beta

--- citystatistic/src/components/MyProfile.js
+++ citystatistic/src/components/MyProfile.js
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../MyProfile.css";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
 export default function MyProfile() {
-  return (
-    <div className="body-container">
-      <aside class="profile-card">
-        <header>
-          <a href="http://victory-design.ru/">
-            <img src="http://victory-design.ru/sandbox/codepen/profile_card/avatar.svg" />
-          </a>
+  const [cookies, setCookie, removeCookie] = useCookies(["auth"]);
+  const [profileData, setProfileData] = useState([]);
 
-          <h1>Sriijon Sarker</h1>
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:8080/profile",
+      headers: { Authorization: `Bearer ${cookies["auth"]}` },
+      data: {},
+    })
+      .then((res) => {
+        setProfileData((oldData) => [...oldData, res.data]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  console.log(profileData[0]);
+  if (profileData.length < 1) {
+    return "Hali";
+  } else {
+    return (
+      <div className="body-container">
+        <aside class="profile-card">
+          <header>
+            <a href="#">
+              <img src="http://victory-design.ru/sandbox/codepen/profile_card/avatar.svg" />
+            </a>
 
-          <h2>Web Developer</h2>
-        </header>
+            <h1>{profileData[0].username}</h1>
 
-        <div class="profile-bio">
-          <p>
-            Even when everything is perfect, you can always make it better.
-            Break barriers in your head, create something crazy and don't forget
-            Code is Poetry...
-          </p>
-        </div>
-      </aside>
-    </div>
-  );
+            <h2>{profileData[0].email}</h2>
+          </header>
+
+          <div class="profile-bio">
+            <p>
+              Under construction... <br /> Please come back later!
+            </p>
+          </div>
+        </aside>
+      </div>
+    );
+  }
 }
