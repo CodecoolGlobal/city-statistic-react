ad4fa12a8295a94acd144842b7aea53f03107385
PeterBorok <peter.borok@gmail.com>
2020-08-27 16:14:12 +0200

    add comment-reply function (raw version)

--- citystatistic/src/components/Comments.js
+++ citystatistic/src/components/Comments.js
@@ -6,6 +6,17 @@ import axios from "axios";
 
 
 export default function Comments(props) {
+    let [replyContent, setReplyContent] = useState("");
+
+    function handleReply(e) {
+
+    }
+
+    function handleChange(e) {
+        e.preventDefault();
+        setReplyContent((e.currentTarget).value)
+
+    }
 
     function handleUpvote(e) {
         let element = document.getElementById(e.currentTarget.closest("div").dataset.id)
@@ -26,6 +37,16 @@ export default function Comments(props) {
         axios.put('http://localhost:8080/rate/' + id, {rate: "downvote"}).then(res => console.log(res))
     }
 
+    function handleReplySubmit(e) {
+        e.preventDefault()
+        let id = e.currentTarget.dataset.id
+        console.log(e.currentTarget)
+        if (!replyContent == "") {
+            axios.post('http://localhost:8080/reply/' + id, {reply: replyContent}).then(res => console.log(res))
+        }
+        e.target.reset();
+    }
+
     return (
         <div>
             <ul id="comment-container" className="comments">
@@ -35,7 +56,7 @@ export default function Comments(props) {
                             <img className="user-avatar"
                                  src="https://library.kissclipart.com/20180904/ese/kissclipart-user-icon-png-clipart-computer-icons-user-66fe7db07b02eb73.jpg"/>
                             <div className="user-info">
-                                <h4 className="user__name">{}</h4>
+                                <h4 className="user__name">{comment.appuser.username}</h4>
                                 <div className="user__comment">
                                     {comment.comment}</div>
                                 <div className="user__comment__reaction">
@@ -46,6 +67,21 @@ export default function Comments(props) {
                                         <Icon icon={thumbsDown} color="red" width="24"/> <small id = {comment.id + "down"}>{comment.downvote}</small>
                                     </div>
                                 </div>
+                                <button onClick={handleReply}>Reply</button>
+                                <div className="replies">
+                                    <ul>
+                                        {comment.replies.map(function (reply){
+                                            return <li>
+                                                <p>{reply}</p>
+                                            </li>
+                                        })}
+                                    </ul>
+                                </div>
+                                <form id="commentForm" onSubmit={handleReplySubmit} data-id = {comment.id}>
+                                    <textarea id={"replyTextArea" + comment.id} onChange={handleChange} placeholder="Share your opinion..."></textarea>
+                                    <button id="submit" type="submit">Submit
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </li>
